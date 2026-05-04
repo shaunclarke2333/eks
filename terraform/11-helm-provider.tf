2f9a1af2377b5f2b89b381ebdd34ac7e5db2d37d
Shaun <shaunclarke43@gmail.com>
2026-05-03 18:03:48 -0700

    added cluster autoscaler

--- terraform/11-helm-provider.tf
+++ terraform/11-helm-provider.tf
@@ -1,19 +1,19 @@
 # Creating data resource to retrieve the cluster name
 data "aws_eks_cluster" "eks" {
-    name = aws_eks_cluster.eks.name
+  name = aws_eks_cluster.eks.name
 }
 
 # Creating a data resource to get teh name of the cluster authentication 
 data "aws_eks_cluster_auth" "eks" {
-    name = aws_eks_cluster.eks.name
-  
+  name = aws_eks_cluster.eks.name
+
 }
 
 # Resource block that initilizes helm
 provider "helm" {
   kubernetes = {
-    host = aws_eks_cluster.eks.endpoint
+    host                   = aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token = data.aws_eks_cluster_auth.eks.token
+    token                  = data.aws_eks_cluster_auth.eks.token
   }
 }
